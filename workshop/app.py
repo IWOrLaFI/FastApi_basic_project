246cbd74cb24992aed064b6e6809aebfd1bd6b66
IWOrLaFI <wolf6961@gmail.com>
2022-07-08 11:42:12 +0300

    edit docker-compose.yml

--- workshop/app.py
+++ workshop/app.py
@@ -1,21 +1,33 @@
 from fastapi import FastAPI
 from .routes import test_routes
-from workshop.config import PROJECT_NAME, VERSION
+from config import PROJECT_NAME, VERSION
 from fastapi.middleware.cors import CORSMiddleware
+from workshop.db.db import get_db
 
-origins = ["http://localhost:8080", "http://127.0.0.1:8080"]
+ORIGIN = ["http://localhost:8080", "http://127.0.0.1:8080"]
 
 
 def get_application():
     _app = FastAPI(title=PROJECT_NAME, version=VERSION)
     _app.add_middleware(CORSMiddleware,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-        )
+                        allow_credentials=True,
+                        allow_methods=["*"],
+                        allow_headers=["*"],
+                        )
     return _app
 
 
 app = get_application()
-
 app.include_router(test_routes.router)
+db = get_db()
+
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+    print('Success')
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
---
+++ workshop/db/db.py
@@ -0,0 +1,23 @@
+import databases
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:changeme@postgress:5432'
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    _db = databases.Database(SQLALCHEMY_DATABASE_URL)
+    return _db
+
+
+
+
