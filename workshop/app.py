1607cacc04796b83d48e8e91f617330317325090
IWOrLaFI <wolf6961@gmail.com>
2022-07-12 11:38:44 +0300

    repeir Docker-Compose

--- workshop/app.py
+++ workshop/app.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from .routes import test_routes, auth
 from workshop.config import PROJECT_NAME, VERSION
 from fastapi.middleware.cors import CORSMiddleware
-
-origins = ["http://localhost:8080", "http://127.0.0.1:8080"]
+from .db.database import get_db
+ORIGINS = ["http://localhost:8080", "http://127.0.0.1:8080"]
 
 
 def get_application():
     _app = FastAPI(title=PROJECT_NAME, version=VERSION)
-    _app.add_middleware(CORSMiddleware,
+    _app.add_middleware(
+        CORSMiddleware,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
@@ -20,3 +21,16 @@ app = get_application()
 
 app.include_router(test_routes.router)
 app.include_router(auth.router)
+
+db = get_db()
+
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+    print('Success')
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
--- workshop/settings.py
+++ workshop/settings.py
@@ -4,9 +4,11 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 8000
+    # db_url: str = 'postgresql://postgres:changeme@postgress:5432'
+    db_url: str = 'postgresql://fastapi_basic_project:fastapi_user@db:5432/fastapi_basic_project'
 
 
 settings = Settings(
     _env_file='.env',
     _env_file_encoding='utf-8',
-)
+    )
