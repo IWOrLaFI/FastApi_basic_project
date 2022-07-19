25b1f5c049fb9a8d761f33e3cf9e585be023fd54
IWOrLaFI <wolf6961@gmail.com>
2022-07-19 21:44:35 +0300

    added models/quiz.py

---
+++ workshop/models/quiz.py
@@ -0,0 +1,34 @@
+import datetime
+from typing import Optional
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    validator,
+    constr
+    )
+
+
+class QuizResult(BaseModel):
+    id: Optional[int] = None
+    user_id = int
+    date = str
+    result = int
+    user_answer_list = list
+    description = str
+
+    class Config:
+        orm_mode = True
+
+
+class Quiz(BaseModel):
+    __tablename__ = 'Quiz'
+
+    id_question: Optional[int] = None
+    question = str
+    answer = str
+
+class QuizAnswer(Base):
+    __tablename__ = 'QuizAnswer'
+
+    id_question = int
+    answer_correct = str
