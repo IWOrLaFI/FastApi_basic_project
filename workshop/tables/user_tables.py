b169310af339247be3f6fdd56f4f1d35b98848b2
IWOrLaFI <wolf6961@gmail.com>
2022-07-19 14:08:11 +0300

    added tables/quiz_tables.py

--- workshop/tables/user_tables.py
+++ workshop/tables/user_tables.py
@@ -20,21 +20,3 @@ class User(Base):
     username = Column(String, unique=True) #email
     password_hash = Column(String)
 
-
-class Result(Base):
-    __tablename__ = 'results'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), index=True)
-    date = Column(String)
-    result = Column(Integer)
-    answer_list = Column(String)
-    description = Column(String, nullable=True)
-
-
-class Quiz(Base):
-    __tablename__ = 'Quiz'
-
-    id_question = Column(Integer, primary_key=True, unique=True)
-    question = Column(String)
-    answer = Column(String)
