ba651155316922d39c9f6db38bfecbad1edead51
IWOrLaFI <wolf6961@gmail.com>
2022-07-21 16:05:05 +0300

    edit all

---
+++ workshop/database/models/quiz_tables.py
@@ -0,0 +1,35 @@
+from sqlalchemy import (
+    Column,
+    Date,
+    ForeignKey,
+    Integer,
+    String,
+    )
+
+from workshop.database.db import Base
+
+
+class QuizResult(Base):
+    __tablename__ = 'QuizResult'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
+    date = Column(String)
+    result = Column(Integer)
+    answer_list = Column(String)
+    description = Column(String, nullable=True)
+
+
+class Quiz(Base):
+    __tablename__ = 'Quiz'
+
+    id_question = Column(Integer, primary_key=True, unique=True)
+    question = Column(String)
+    answer = Column(String)
+
+
+class QuizAnswer(Base):
+    __tablename__ = 'QuizAnswer'
+
+    id_question = Column(Integer, ForeignKey('quiz.id'), index=True)
+    answer_correct = Column(String)
